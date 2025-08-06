22b42e2126da8d30457f65fa1331989e88ac0874
chariot27 <maxtarigodev@gmail.com>
2025-08-06 20:11:33 -0300

    sec reabilitada para com permissão externa para teste

--- src/main/java/br/ars/user_service/config/SecurityConfig.java
+++ src/main/java/br/ars/user_service/config/SecurityConfig.java
@@ -2,11 +2,24 @@ package br.ars.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // desativa CSRF (recomendado para APIs REST)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // PERMITE TODAS AS ROTAS
+            );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
