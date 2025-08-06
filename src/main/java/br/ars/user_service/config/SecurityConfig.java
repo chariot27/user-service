3ed51b1f550479e7066cfc7fbd9093c7a623188b
chariot27 <maxtarigodev@gmail.com>
2025-08-06 19:58:18 -0300

    atualização do tratativa de criptografia de senhas

--- src/main/java/br/ars/user_service/config/SecurityConfig.java
+++ src/main/java/br/ars/user_service/config/SecurityConfig.java
@@ -2,33 +2,11 @@ package br.ars.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()) // desabilita CSRF (ideal para APIs)
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // sem sessão
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    new AntPathRequestMatcher("/auth/**"),  // registro, login
-                    new AntPathRequestMatcher("/swagger-ui/**"),
-                    new AntPathRequestMatcher("/v3/api-docs/**")
-                ).permitAll()
-                .anyRequest().authenticated()
-            );
-
-        return http.build();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
